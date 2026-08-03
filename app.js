291b649eb80bfd1366b285d33b992b6de13770e6
朱亮 <zhuliang.12@bytedance.com>
2026-08-03 23:12:26 +0800

    Stabilize colorful map tile loading
    Change-Id: I2a8a69df205c3dff90d3623710e3e4eb6d916f4e

--- app.js
+++ app.js
@@ -28,9 +28,10 @@
       zoomDelta: 0.5
     }).setView([48, 55], 2.25);
     L.control.zoom({ position: "bottomright" }).addTo(atlasMap);
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://b.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png", {
       maxZoom: 19,
       maxNativeZoom: 18,
+      detectRetina: false,
       updateWhenIdle: true,
       updateWhenZooming: false,
       keepBuffer: 1,
@@ -223,9 +224,10 @@
     if (!mapEl || !window.L) return;
     const map = L.map(mapEl, { zoomControl: false, scrollWheelZoom: true, preferCanvas: true }).setView([63.2, 10.3], 5);
     L.control.zoom({ position: "bottomright" }).addTo(map);
-    L.tileLayer("https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png", {
+    L.tileLayer("https://b.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png", {
       maxZoom: 19,
       maxNativeZoom: 18,
+      detectRetina: false,
       updateWhenIdle: true,
       updateWhenZooming: false,
       keepBuffer: 1,
